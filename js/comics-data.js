a15baa7b735c082af76773253aa2bc2472854bc2
DokterGigiGAUL <doktergigigaul@gmail.com>
2026-07-07 16:17:36 +0700

    Create comics-data.js

---
+++ js/comics-data.js
@@ -0,0 +1,20 @@
+const comics = [
+  {
+    id: 1,
+    title: "Komik #1",
+    thumb: "assets/comics/komik1/thumb.webp",
+    image: "assets/comics/komik1/comic.webp"
+  },
+  {
+    id: 2,
+    title: "Komik #2",
+    thumb: "assets/comics/komik2/thumb.webp",
+    image: "assets/comics/komik2/comic.webp"
+  },
+  {
+    id: 3,
+    title: "Komik #3",
+    thumb: "assets/comics/komik3/thumb.webp",
+    image: "assets/comics/komik3/comic.webp"
+  }
+];
