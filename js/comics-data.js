635e4bc2cc51c3d3a7a5050aab03a212bcdb3a89
DokterGigiGAUL <doktergigigaul@gmail.com>
2026-07-07 18:00:12 +0700

    Update comics-data.js

--- js/comics-data.js
+++ js/comics-data.js
@@ -2,19 +2,19 @@ const comics = [
   {
     id: 1,
     title: "Komik #1",
-    thumb: "assets/comics/komik1/thumb.webp",
-    image: "assets/comics/komik1/comic.webp"
+    thumb: "assets/comics/komik/IMG_1905.jpeg",
+    image: "assets/comics/komik1/IMG_1905.jpeg"
   },
   {
     id: 2,
     title: "Komik #2",
-    thumb: "assets/comics/komik2/thumb.webp",
-    image: "assets/comics/komik2/comic.webp"
+    thumb: "assets/comics/komik1/IMG_1905.jpeg",
+    image: "assets/comics/komik1/IMG_1905.jpeg"
   },
   {
     id: 3,
     title: "Komik #3",
-    thumb: "assets/comics/komik3/thumb.webp",
-    image: "assets/comics/komik3/comic.webp"
+    thumb: "assets/comics/komik1/IMG_1905.jpeg",
+    image: "assets/comics/komik1/IMG_1905.jpeg"
   }
 ];
