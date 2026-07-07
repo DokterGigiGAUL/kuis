1a5559ad2a93c791b3d2aa6958e526bcb931607d
DokterGigiGAUL <doktergigigaul@gmail.com>
2026-07-08 01:06:53 +0700

    Update comics-data.js

--- js/comics-data.js
+++ js/comics-data.js
@@ -1,19 +1,19 @@
 const comics = [
   {
     id: 1,
-    title: "Sariawan Bukan Akibat Infeksi Jamur",
+    title: "Episode #001: Apakah Sariawan Akibat Infeksi Jamur?",
     thumb: "assets/comics/komik1/IMG_1966.jpeg",
     image: "assets/comics/komik1/IMG_1967.webp"
   },
   {
     id: 2,
-    title: "Pentingnya Mengganti Gigi Hilang dengan Gigi Tiruan",
+    title: "Episode #002: Kenapa Gigi Hilang Perlu Diganti?",
     thumb: "assets/comics/komik2/IMG_1966.jpeg",
     image: "assets/comics/komik2/IMG_1970.webp"
   },
   {
     id: 3,
-    title: "Hubungan Merokok dan Kanker Mulut",
+    title: "Episode #003: Bagaimana Hubungan Rokok dan Kanker Mulut?",
     thumb: "assets/comics/komik3/IMG_1966.jpeg",
     image: "assets/comics/komik3/6E4F0001-06FC-4B6F-8D5D-30E39EE23DB8.png"
   }
