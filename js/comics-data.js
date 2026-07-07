90beaa59715dc4dc9a3d71dac18cddfaa908cba6
DokterGigiGAUL <doktergigigaul@gmail.com>
2026-07-07 18:40:03 +0700

    Update comics-data.js

--- js/comics-data.js
+++ js/comics-data.js
@@ -2,19 +2,19 @@ const comics = [
   {
     id: 1,
     title: "Hubungan Merokok dan Kanker Mulut",
-    thumb: "assets/comics/komik/IMG_1905.jpeg",
-    image: "assets/comics/komik1/6E4F0001-06FC-4B6F-8D5D-30E39EE23DB8.png"
+    thumb: "assets/comics/komik1/IMG_1905.jpeg",
+    image: "assets/comics/komik1/IMG_1877.jpeg"
   },
   {
     id: 2,
     title: "Pentingnya Mengganti Gigi Hilang dengan Gigi Tiruan",
-    thumb: "assets/comics/komik1/IMG_1905.jpeg",
-    image: "assets/comics/komik1/6E4F0001-06FC-4B6F-8D5D-30E39EE23DB8.png"
+    thumb: "assets/comics/komik2/IMG_1905.jpeg",
+    image: "assets/comics/komik2/6E4F0001-06FC-4B6F-8D5D-30E39EE23DB8.png"
   },
   {
     id: 3,
     title: "Hubungan Penyakit Periodontal dan Kelahiran Prematur",
-    thumb: "assets/comics/komik1/IMG_1905.jpeg",
-    image: "assets/comics/komik1/6E4F0001-06FC-4B6F-8D5D-30E39EE23DB8.png"
+    thumb: "assets/comics/komik3/IMG_1905.jpeg",
+    image: "assets/comics/komik3/6E4F0001-06FC-4B6F-8D5D-30E39EE23DB8.png"
   }
 ];
