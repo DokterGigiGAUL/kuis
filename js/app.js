d72c23f8616986b4f2877c7cc180ea992b913497
DokterGigiGAUL <doktergigigaul@gmail.com>
2026-07-07 18:21:12 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -82,7 +82,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
               <a
                 href="komik.html?id=${comic.id}"
-                class="comic-button"
+                class="quiz-button"
               >
                 Baca Komik
               </a>
