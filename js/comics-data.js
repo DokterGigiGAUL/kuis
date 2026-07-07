202e1340be16a739094d057036430ab279c19bfc
DokterGigiGAUL <doktergigigaul@gmail.com>
2026-07-07 21:52:29 +0700

    Update comics-data.js

--- js/comics-data.js
+++ js/comics-data.js
@@ -9,7 +9,7 @@ const comics = [
     id: 2,
     title: "Pentingnya Mengganti Gigi Hilang dengan Gigi Tiruan",
     thumb: "assets/comics/komik2/IMG_1905.jpeg",
-    image: "assets/comics/komik1/IMG_1964.webp"
+    image: "assets/comics/komik2/6E4F0001-06FC-4B6F-8D5D-30E39EE23DB8.png"
   },
   {
     id: 3,
