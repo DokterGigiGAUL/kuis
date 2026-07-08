c9f0a75dc7a396303ab96b88686095133fa44976
DokterGigiGAUL <doktergigigaul@gmail.com>
2026-07-08 11:45:46 +0700

    Update comics-data.js

--- js/comics-data.js
+++ js/comics-data.js
@@ -13,7 +13,7 @@ const comics = [
   },
   {
     id: 3,
-    title: "Hubungan Rokok dan Kanker Mulut",
+    title: "Hubungan Merokok dan Kanker Mulut",
     thumb: "assets/comics/komik3/IMG_1966.jpeg",
     image: "assets/comics/komik3/6E4F0001-06FC-4B6F-8D5D-30E39EE23DB8.png"
   }
