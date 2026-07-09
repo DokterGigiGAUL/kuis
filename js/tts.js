3d247f24b583da159896ab05e81f0c3edb3ed133
DokterGigiGAUL <doktergigigaul@gmail.com>
2026-07-09 21:43:51 +0700

    Create tts.js

---
+++ js/tts.js
@@ -0,0 +1,35 @@
+const ttsList = [
+
+    {
+        id: 1,
+        title: "TTS #1",
+        description: "Teka-teki silang seputar penyakit mulut dengan soal yang dipilih secara acak.",
+        soal: 20,
+        thumb: "assets/images/tts/tts1.webp"
+    },
+
+    {
+        id: 2,
+        title: "TTS #2",
+        description: "Teka-teki silang seputar penyakit mulut dengan soal yang dipilih secara acak.",
+        soal: 25,
+        thumb: "assets/images/tts/tts2.webp"
+    },
+
+    {
+        id: 3,
+        title: "TTS #3",
+        description: "Teka-teki silang seputar penyakit mulut dengan soal yang dipilih secara acak.",
+        soal: 30,
+        thumb: "assets/images/tts/tts3.webp"
+    },
+
+    {
+        id: 4,
+        title: "TTS #4",
+        description: "Teka-teki silang seputar penyakit mulut dengan soal yang dipilih secara acak.",
+        soal: 20,
+        thumb: "assets/images/tts/tts4.webp"
+    }
+
+];
