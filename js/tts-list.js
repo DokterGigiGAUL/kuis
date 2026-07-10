f34fc54fcf12e254aeeb71423b2cc0f7d08015a6
DokterGigiGAUL <doktergigigaul@gmail.com>
2026-07-10 15:11:08 +0700

    Update tts-list.js

--- js/tts-list.js
+++ js/tts-list.js
@@ -3,7 +3,7 @@ const ttsList = [
     {
         id: 1,
         title: "TTS #1",
-        description: "Teka-teki silang Interaktif.",
+        description: "Teka-teki silang interaktif.",
         soal: 18,
         thumb: "assets/images/tts/C786BC7F-557F-4325-A40F-1D9BCB141C58.png"
     },
@@ -11,7 +11,7 @@ const ttsList = [
     {
         id: 2,
         title: "TTS #2",
-        description: "Teka-teki silang Interaktif.",
+        description: "Teka-teki silang interaktif.",
         soal: 10,
         thumb: "assets/images/tts/C786BC7F-557F-4325-A40F-1D9BCB141C58.png"
     },
@@ -19,16 +19,16 @@ const ttsList = [
     {
         id: 3,
         title: "TTS #3",
-        description: "Teka-teki silang Interaktif.",
-        soal: 30,
+        description: "Teka-teki silang interaktif.",
+        soal: 12,
         thumb: "assets/images/tts/C786BC7F-557F-4325-A40F-1D9BCB141C58.png"
     },
 
     {
         id: 4,
         title: "TTS #4",
-        description: "Teka-teki silang Interaktif",
-        soal: 20,
+        description: "Teka-teki silang interaktif",
+        soal: 10,
         thumb: "assets/images/tts/C786BC7F-557F-4325-A40F-1D9BCB141C58.png"
     }
 
