6fa5d84e975957ce6058b55a75a9155695b68d71
DokterGigiGAUL <doktergigigaul@gmail.com>
2026-07-13 13:14:28 +0700

    Update explore.js

--- js/explore.js
+++ js/explore.js
@@ -153,7 +153,7 @@ function showTTS() {
             tts.description;
         card.querySelector(".tts-soal").textContent =
             `${tts.soal} Soal`;
-        card.querySelector(".comic-btn").onclick = () => {
+        card.querySelector(".tts-btn").onclick = () => {
             location.href =
                 `assets/tts/tts${tts.id}.html`;
         };
