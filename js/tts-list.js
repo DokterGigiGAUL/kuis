bc7a9cb03db7a1c283a0606551d21fd449daca10
DokterGigiGAUL <doktergigigaul@gmail.com>
2026-07-14 17:14:02 +0700

    Update tts-list.js

--- js/tts-list.js
+++ js/tts-list.js
@@ -4,28 +4,28 @@ const ttsList = [
         id: 1,
         title: "TTS #1",
         soal: 12,
-        thumb: "assets/images/tts/Tts1.webp"
+        thumb: "assets/images/tts/IMG_4268.jpeg"
     },
 
     {
         id: 2,
         title: "TTS #2",
         soal: 11,
-        thumb: "assets/images/tts/Tts2.webp"
+        thumb: "assets/images/tts/IMG_4268.jpeg"
     },
 
     {
         id: 3,
         title: "TTS #3",
         soal: 12,
-        thumb: "assets/images/tts/Tts3.webp"
+        thumb: "assets/images/tts/IMG_4268.jpeg"
     },
 
     {
         id: 4,
         title: "TTS #4",
         soal: 20,
-        thumb: "assets/images/tts/Tts4.webp"
+        thumb: "assets/images/tts/IMG_4268.jpeg"
     }
 
 ];
