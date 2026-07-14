97e68a32d53c87c0bf5b4c6b0900f157425d76c4
DokterGigiGAUL <doktergigigaul@gmail.com>
2026-07-15 01:00:09 +0700

    Update comics-data.js

--- js/comics-data.js
+++ js/comics-data.js
@@ -1,6 +1,10 @@
-const comics = [
-  {
+const comics = [{
+
   id: 1,
+  productId: "komik1",
+  premium: false,
+  price: 0,
+  
   title: "Sariawan Karena Infeksi Jamur?",
   thumb: "assets/comics/komik1/IMG_1966.jpeg",
   images: [
@@ -11,6 +15,10 @@ const comics = [
   },
   {
   id: 2,
+  productId: "komik1",
+  premium: false,
+  price: 0,
+    
   title: "Kenapa Gigi Hilang Perlu Diganti?",
   thumb: "assets/comics/komik2/IMG_1966.jpeg",
 
@@ -21,7 +29,25 @@ const comics = [
   },
   {
   id: 3,
+  productId: "komik1",
+  premium: false,
+  price: 0,
+    ,
   title: "Hubungan Merokok dan Kanker Mulut",
+  thumb: "assets/comics/komik3/IMG_1966.jpeg",
+
+  images: [
+    "assets/comics/komik3/6E4F0001-06FC-4B6F-8D5D-30E39EE23DB8.png",
+    "assets/comics/komik3/6E4F0001-06FC-4B6F-8D5D-30E39EE23DB8.png"
+  ]
+  },
+  {
+  id: 4,
+  productId: "komik1",
+  premium: true,
+  price: 5000,
+    ,
+  title: "Gusi Berdarah Berbahaya Gak?",
   thumb: "assets/comics/komik3/IMG_1966.jpeg",
 
   images: [
