4b4f3a9eed8bd04d763600b590fbb201e870f531
DokterGigiGAUL <doktergigigaul@gmail.com>
2026-07-14 10:36:13 +0700

    Update tts-list.js

--- js/tts-list.js
+++ js/tts-list.js
@@ -3,7 +3,6 @@ const ttsList = [
     {
         id: 1,
         title: "TTS #1",
-        description: "Topik: Penyakit Mulut",
         soal: 12,
         thumb: "assets/images/tts/IMG_4268.jpeg"
     },
@@ -11,7 +10,6 @@ const ttsList = [
     {
         id: 2,
         title: "TTS #2",
-        description: "Topik: Kedokteran Gigi",
         soal: 11,
         thumb: "assets/images/tts/IMG_4268.jpeg
     },
@@ -19,7 +17,6 @@ const ttsList = [
     {
         id: 3,
         title: "TTS #3",
-        description: "Topik: Kedokteran Gigi",
         soal: 12,
         thumb: "assets/images/tts/Tts3.webp"
     },
@@ -27,7 +24,6 @@ const ttsList = [
     {
         id: 4,
         title: "TTS #4",
-        description: "Teka-teki Premium",
         soal: 20,
         thumb: "assets/images/tts/Tts4.webp"
     }
