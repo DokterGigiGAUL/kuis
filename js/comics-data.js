2a0817a0f2e2e567606e41d3e446f8c9a56a797f
DokterGigiGAUL <doktergigigaul@gmail.com>
2026-07-14 23:00:02 +0700

    Update comics-data.js

--- js/comics-data.js
+++ js/comics-data.js
@@ -1,20 +1,32 @@
 const comics = [
   {
-    id: 1,
-    title: "Sariawan Karena Infeksi Jamur?",
-    thumb: "assets/comics/komik1/IMG_1966.jpeg",
-    image: "assets/comics/komik1/IMG_1967.webp"
+  id: 1,
+  title: "Sariawan Karena Infeksi Jamur?",
+  thumb: "assets/comics/komik1/IMG_1966.jpeg",
+  images: [
+    "assets/comics/komik1/IMG_1967.webp",
+    "assets/comics/komik1/IMG_1964.webp",
+    "assets/comics/komik1/IMG_1877.jpeg"
+  ]
   },
   {
-    id: 2,
-    title: "Kenapa Gigi Hilang Perlu Diganti?",
-    thumb: "assets/comics/komik2/IMG_1966.jpeg",
-    image: "assets/comics/komik2/IMG_1970.webp"
+  id: 2,
+  title: "Kenapa Gigi Hilang Perlu Diganti?",
+  thumb: "assets/comics/komik2/IMG_1966.jpeg",
+
+  images: [
+    "assets/comics/komik2/IMG_1970.webp",
+    "assets/comics/komik2/DB727B23-FAAD-45B5-A560-2A20DEBDFE27.png",
+  ]
   },
   {
-    id: 3,
-    title: "Hubungan Merokok dan Kanker Mulut",
-    thumb: "assets/comics/komik3/IMG_1966.jpeg",
-    image: "assets/comics/komik3/6E4F0001-06FC-4B6F-8D5D-30E39EE23DB8.png"
+  id: 3,
+  title: "Hubungan Merokok dan Kanker Mulut",
+  thumb: "assets/comics/komik3/IMG_1966.jpeg",
+
+  images: [
+    "assets/comics/komik3/6E4F0001-06FC-4B6F-8D5D-30E39EE23DB8.png",
+    "assets/comics/komik3/6E4F0001-06FC-4B6F-8D5D-30E39EE23DB8.png",
+  ]
   }
 ];
