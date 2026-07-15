1ae73b2b5f9742f4553e1b1eb0d1a1c26555b851
DokterGigiGAUL <doktergigigaul@gmail.com>
2026-07-16 00:07:25 +0700

    Update case-data.js

--- js/case-data.js
+++ js/case-data.js
@@ -1,6 +1,23 @@
-const caseFiles = [
-    "case1",
-    "case2",
-    "case3",
-    "case4"
+const cases = [
+    {
+        id: 1,
+        file: "case1",
+        title: "Kasus #1",
+        thumb: "assets/images/cases/case1.jpg",
+        premium: false
+    },
+    {
+        id: 2,
+        file: "case2",
+        title: "Kasus #2",
+        thumb: "assets/images/cases/case2.jpg",
+        premium: true
+    },
+    {
+        id: 3,
+        file: "case3",
+        title: "Kasus #3",
+        thumb: "assets/images/cases/case3.jpg",
+        premium: true
+    }
 ];
