6a9699cec6263702716631c2ede0bba036a85512
DokterGigiGAUL <doktergigigaul@gmail.com>
2026-07-15 22:58:47 +0700

    Update case.js

--- js/case.js
+++ js/case.js
@@ -1,5 +1,16 @@
-const flashcard = document.getElementById("flashcard");
+const caseData = {
+    image: "assets/cases/images/case1.jpg",
+    gender: "Perempuan",
+    age: "43 tahun",
+    anamnesis: "Pasien datang dengan keluhan luka putih pada mukosa pipi kanan sejak tiga minggu yang lalu. Tidak terasa nyeri.",
+    diagnosis: "ORAL LICHEN PLANUS"
+};
 
-flashcard.addEventListener("click", () => {
-    flashcard.classList.toggle("flipped");
+document.getElementById("case-image").src = caseData.image;
+document.getElementById("patient").textContent = `👤 ${caseData.gender}, ${caseData.age}`;
+document.getElementById("anamnesis").textContent = caseData.anamnesis;
+document.getElementById("diagnosis").textContent = caseData.diagnosis;
+
+document.getElementById("flashcard").addEventListener("click", () => {
+    document.getElementById("flashcard").classList.toggle("flipped");
 });
