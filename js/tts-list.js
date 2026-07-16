12f23861708ba7b94afddd3a7d25b1d211f82a8e
DokterGigiGAUL <doktergigigaul@gmail.com>
2026-07-16 11:12:47 +0700

    Update tts-list.js

--- js/tts-list.js
+++ js/tts-list.js
@@ -5,7 +5,7 @@ const ttsList = [
         title: "TTS #1",
         productId: "tts1",
         soal: 12,
-        thumb: "assets/images/tts/D64390B9-FFB2-4D60-8BA0-9F714360822E.png",
+        thumbnail: "assets/images/tts/D64390B9-FFB2-4D60-8BA0-9F714360822E.png",
         premium: false,
         price: 0
     },
@@ -15,7 +15,7 @@ const ttsList = [
         title: "TTS #2",
         productId: "tts2",
         soal: 11,
-        thumb: "assets/images/tts/D64390B9-FFB2-4D60-8BA0-9F714360822E.png",
+        thumbnail: "assets/images/tts/D64390B9-FFB2-4D60-8BA0-9F714360822E.png",
         premium: false,
         price: 0
     },
@@ -25,7 +25,7 @@ const ttsList = [
         title: "TTS 3",
         productId: "tts3",
         soal: 12,
-        thumb: "assets/images/tts/D64390B9-FFB2-4D60-8BA0-9F714360822E.png",
+        thumbnail: "assets/images/tts/D64390B9-FFB2-4D60-8BA0-9F714360822E.png",
         premium: false,
         price: 0
     },
@@ -35,7 +35,7 @@ const ttsList = [
         title: "TTS #4",
         productId: "tts4",
         soal: 20,
-        thumb: "assets/images/tts/D64390B9-FFB2-4D60-8BA0-9F714360822E.png",
+        thumbnail: "assets/images/tts/D64390B9-FFB2-4D60-8BA0-9F714360822E.png",
         premium: true,
         price: 5000
     }
