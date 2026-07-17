5443786a084230e378e80838d5e90cdf6702e762
DokterGigiGAUL <doktergigigaul@gmail.com>
2026-07-18 02:26:04 +0700

    Update case-data.js

--- js/case-data.js
+++ js/case-data.js
@@ -3,6 +3,7 @@ const cases = [
         id: 1,
         file: "case1",
         title: "Kasus #1",
+        description: "Apa diagnosisnya?",
         thumbnail: "assets/images/cases/case1.jpg",
         premium: false,
         price: 0
@@ -11,6 +12,7 @@ const cases = [
         id: 2,
         file: "case2",
         title: "Kasus #2",
+        description: "Apa diagnosisnya?",
         thumbnail: "assets/images/cases/case2.jpg",
         premium: false,
         price: 0
@@ -19,6 +21,7 @@ const cases = [
         id: 3,
         file: "case3",
         title: "Kasus #3",
+        description: "Apa diagnosisnya?",
         thumbnail: "assets/images/cases/case3.jpg",
         premium: false,
         price: 0
@@ -27,6 +30,7 @@ const cases = [
         id: 4,
         file: "case4",
         title: "Kasus #4",
+        description: "Apa diagnosisnya?",
         thumbnail: "assets/images/cases/case4.jpg",
         premium: true,
         price: 5000
