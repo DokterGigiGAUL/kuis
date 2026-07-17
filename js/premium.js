689961b347785baffafee7d34d9997d70e7e79cc
DokterGigiGAUL <doktergigigaul@gmail.com>
2026-07-17 19:35:08 +0700

    Create premium.js

---
+++ js/premium.js
@@ -0,0 +1,8 @@
+function showPremiumDialog() {
+    alert(
+        "🔒 Konten Premium\n\n" +
+        "Konten ini merupakan bagian dari Wonder App Premium.\n\n" +
+        "Saat ini akses Premium belum dapat diakses karena masih dalam tahap sosialisasi awal.\n\n" +
+        "Terima kasih atas dukungannya 😊"
+    );
+}
