ab52ea8182e375c2eb4810ef373a73b4d638d6e4
DokterGigiGAUL <doktergigigaul@gmail.com>
2026-07-25 11:59:32 +0700

    Create ebook-data.js

---
+++ js/ebook-data.js
@@ -0,0 +1,11 @@
+const ebooks = [
+    {
+        id: 1,
+        title: "Panduan Peresepan Dokter Gigi Pasien Anak",
+        description: "Panduan praktis pemilihan obat dan peresepan pada pasien anak.",
+        thumbnail: "assets/images/ebook/ebook1.jpg",
+        price: 54321,
+        mayarUrl: "https://gigital.myr.id/pl/ebook-gigital-panduan-peresepan-dokter-gigi-pasien-anak",
+        releaseDate: "2026-07-25"
+    }
+];
