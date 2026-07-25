9e8bed24bfd9111eb98ec884bccaae22b364ce1f
DokterGigiGAUL <doktergigigaul@gmail.com>
2026-07-25 13:47:51 +0700

    Update ebook-data.js

--- js/ebook-data.js
+++ js/ebook-data.js
@@ -1,8 +1,9 @@
 const ebooks = [
     {
         id: 1,
+        file: "ebook1",
         title: "Panduan Peresepan Dokter Gigi",
-        description: "Buku kumpulan panduan peresepan dari obat-obatan yang umum diresepkan pada terapi penyakit gigi dan mulut. ",
+        description: "Panduan peresepan dari obat-obatan yang umum diresepkan pada terapi penyakit gigi dan mulut. ",
         /*thumbnail: "assets/images/ebook/ebook1.jpg",*/
         thumbnail: "https://media-myr.b-cdn.net/images/resized/600/6a709c40-0029-4cdc-b274-fe862da5f260.jpeg",
         releaseDate: "2026-06-25",
@@ -11,11 +12,12 @@ const ebooks = [
         "https://media-myr.b-cdn.net/images/resized/600/6a709c40-0029-4cdc-b274-fe862da5f260.jpeg",
         "https://media-myr.b-cdn.net/images/resized/600/6a709c40-0029-4cdc-b274-fe862da5f260.jpeg"
         ],
-        price: 54321,
+        price: 56789,
         mayarUrl: "https://gigital.myr.id/pl/ebook-gigital-panduan-peresepan-dokter-gigi"
     }
     {
         id: 2,
+        file: "ebook2",
         title: "Panduan Peresepan Dokter Gigi Pasien Anak",
         description: "Panduan praktis pemilihan obat dan peresepan pada pasien anak.",
         /*thumbnail: "assets/images/ebook/ebook2.jpg",*/
@@ -27,6 +29,70 @@ const ebooks = [
         "https://media-myr.b-cdn.net/images/resized/600/98d4e9e7-cf62-4e0a-b736-f49f9928c2db.webp"
         ],
         price: 54321,
-        mayarUrl: "https://gigital.myr.id/pl/ebook-gigital-panduan-peresepan-dokter-gigi-pasien-anak"
+        mayarUrl: "https://gigital.myr.id/pl/ebook-gigital-panduan-peresepan-dokter-gigi-pasien-anak?"
+    }
+{
+        id: 3,
+        file: "ebook3",
+        title: "Manajemen Dental Kehamilan",
+        description: "Panduan manajemen dental, meliputi seleksi tindakan dan obat untuk terapi-- pada kehamilan.",
+        /*thumbnail: "assets/images/ebook/ebook2.jpg",*/
+        thumbnail: "https://media-myr.b-cdn.net/images/resized/600/3a6baa2d-38c9-43a0-90c3-167cad21b2b1.jpeg",
+        releaseDate: "2026-06-25",
+        pages: -,
+        preview: [
+        "https://media-myr.b-cdn.net/images/resized/600/3a6baa2d-38c9-43a0-90c3-167cad21b2b1.jpeg",
+        "https://media-myr.b-cdn.net/images/resized/600/3a6baa2d-38c9-43a0-90c3-167cad21b2b1.jpeg"
+        ],
+        price: 45678,
+        mayarUrl: "https://gigital.myr.id/pl/ebook-gigital-panduan-manajemen-dental-kehamilan?"
+    }
+{
+        id: 4,
+        file: "ebook4",
+        title: "Mendiagnosa Lesi Oral Itu Mudah",
+        description: "Panduan praktis diagnosis lesi oral berdasarkan warna, jumlah dan atau bentuk.",
+        /*thumbnail: "assets/images/ebook/ebook2.jpg",*/
+        thumbnail: "https://media-myr.b-cdn.net/images/resized/600/92f1ad8d-e37a-47b7-a74a-0efe416af672.webp",
+        releaseDate: "2026-06-25",
+        pages: -,
+        preview: [
+        "https://media-myr.b-cdn.net/images/resized/600/92f1ad8d-e37a-47b7-a74a-0efe416af672.webp",
+        "https://media-myr.b-cdn.net/images/resized/600/92f1ad8d-e37a-47b7-a74a-0efe416af672.webp"
+        ],
+        price: 43210,
+        mayarUrl: "https://gigital.myr.id/pl/ebook-gigital-mudah-mendiagnosa-lesi-oral?"
+    }
+{
+        id: 5,
+        file: "ebook5",
+        title: "Tatalaksana Lesi Oral",
+        description: "tatalksana lesi oral yang sering ditemui dan menjadi kompetensi dokter gigi umum.",
+        /*thumbnail: "assets/images/ebook/ebook2.jpg",*/
+        thumbnail: "https://media-myr.b-cdn.net/images/resized/600/98236151-2996-436d-91be-5bf8e506b999.jpeg",
+        releaseDate: "2026-06-25",
+        pages: -,
+        preview: [
+        "https://media-myr.b-cdn.net/images/resized/600/98236151-2996-436d-91be-5bf8e506b999.jpeg",
+        "https://media-myr.b-cdn.net/images/resized/600/98236151-2996-436d-91be-5bf8e506b999.jpeg"
+        ],
+        price: 54321,
+        mayarUrl: "https://gigital.myr.id/pl/ebook-gigital-tatalaksana-lesi-oral?"
+    }
+{
+        id: 6,
+        file: "ebook6",
+        title: "Mendiagnosa Lesi Oral Itu Mudah",
+        description: "Contekan ringkas dari handphone panduan pemilihan obat topikal lesi oral.",
+        /*thumbnail: "assets/images/ebook/ebook2.jpg",*/
+        thumbnail: "https://media-myr.b-cdn.net/images/resized/600/40439849-a51a-4bbd-85d3-7b11a69bbb4a.jpeg",
+        releaseDate: "2026-06-25",
+        pages: -,
+        preview: [
+        "https://media-myr.b-cdn.net/images/resized/600/40439849-a51a-4bbd-85d3-7b11a69bbb4a.jpeg",
+        "https://media-myr.b-cdn.net/images/resized/600/40439849-a51a-4bbd-85d3-7b11a69bbb4a.jpeg"
+        ],
+        price: 43210,
+        mayarUrl: "https://gigital.myr.id/pl/ebook-gigital-panduan-pemilihan-obat-topikal?"
     }
 ];
