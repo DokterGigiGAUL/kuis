acb8a6da83fa2abb67eb5cf442143c2c45ca297f
DokterGigiGAUL <doktergigigaul@gmail.com>
2026-07-25 13:31:14 +0700

    Update ebook-data.js

--- js/ebook-data.js
+++ js/ebook-data.js
@@ -1,15 +1,30 @@
 const ebooks = [
     {
         id: 1,
+        title: "Panduan Peresepan Dokter Gigi",
+        description: "Buku kumpulan panduan peresepan dari obat-obatan yang umum diresepkan pada terapi penyakit gigi dan mulut. ",
+        /*thumbnail: "assets/images/ebook/ebook1.jpg",*/
+        thumbnail: "https://media-myr.b-cdn.net/images/resized/600/6a709c40-0029-4cdc-b274-fe862da5f260.jpeg",
+        releaseDate: "2026-06-25",
+        pages: -,
+        preview: [
+        "https://media-myr.b-cdn.net/images/resized/600/6a709c40-0029-4cdc-b274-fe862da5f260.jpeg",
+        "https://media-myr.b-cdn.net/images/resized/600/6a709c40-0029-4cdc-b274-fe862da5f260.jpeg"
+        ],
+        price: 54321,
+        mayarUrl: "https://gigital.myr.id/pl/ebook-gigital-panduan-peresepan-dokter-gigi"
+    }
+    {
+        id: 2,
         title: "Panduan Peresepan Dokter Gigi Pasien Anak",
         description: "Panduan praktis pemilihan obat dan peresepan pada pasien anak.",
-        /*thumbnail: "assets/images/ebook/ebook1.jpg",*/
+        /*thumbnail: "assets/images/ebook/ebook2.jpg",*/
         thumbnail: "https://media-myr.b-cdn.net/images/resized/600/98d4e9e7-cf62-4e0a-b736-f49f9928c2db.webp",
         releaseDate: "2026-06-25",
-        pages: 120,
+        pages: -,
         preview: [
-        "assets/images/ebook/preview1.webp",
-        "assets/images/ebook/preview2.webp"
+        "https://media-myr.b-cdn.net/images/resized/600/98d4e9e7-cf62-4e0a-b736-f49f9928c2db.webp",
+        "https://media-myr.b-cdn.net/images/resized/600/98d4e9e7-cf62-4e0a-b736-f49f9928c2db.webp"
         ],
         price: 54321,
         mayarUrl: "https://gigital.myr.id/pl/ebook-gigital-panduan-peresepan-dokter-gigi-pasien-anak"
