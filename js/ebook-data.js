af94a58c32abf46db9e447ed8dd9f2f8855d794e
DokterGigiGAUL <doktergigigaul@gmail.com>
2026-07-25 13:20:57 +0700

    Update ebook-data.js

--- js/ebook-data.js
+++ js/ebook-data.js
@@ -5,8 +5,13 @@ const ebooks = [
         description: "Panduan praktis pemilihan obat dan peresepan pada pasien anak.",
         /*thumbnail: "assets/images/ebook/ebook1.jpg",*/
         thumbnail: "https://media-myr.b-cdn.net/images/resized/600/98d4e9e7-cf62-4e0a-b736-f49f9928c2db.webp",
+        releaseDate: "2026-06-25",
+        pages: 120,
+        preview: [
+        "assets/images/ebook/preview1.webp",
+        "assets/images/ebook/preview2.webp"
+        ],
         price: 54321,
-        mayarUrl: "https://gigital.myr.id/pl/ebook-gigital-panduan-peresepan-dokter-gigi-pasien-anak",
-        releaseDate: "2026-07-25"
+        mayarUrl: "https://gigital.myr.id/pl/ebook-gigital-panduan-peresepan-dokter-gigi-pasien-anak"
     }
 ];
