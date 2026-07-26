02815c8504ffb3567fff57b0643ec0291044d326
DokterGigiGAUL <doktergigigaul@gmail.com>
2026-07-26 07:29:43 +0700

    Update ebook-data.js

--- js/ebook-data.js
+++ js/ebook-data.js
@@ -13,7 +13,9 @@ const ebooks = [
         "https://media-myr.b-cdn.net/images/resized/600/6a709c40-0029-4cdc-b274-fe862da5f260.jpeg"
         ],
         price: 56789,
-        mayarUrl: "https://gigital.myr.id/pl/ebook-gigital-panduan-peresepan-dokter-gigi"
+        mayarUrl: "https://gigital.myr.id/pl/ebook-gigital-panduan-peresepan-dokter-gigi",
+        samplePdf: "https://drive.google.com/file/d/1N_9nT-0h5EHIDarD1X1k4UHzfrilGgsV/preview"
+
     },
     {
         id: 2,
@@ -29,7 +31,9 @@ const ebooks = [
         "https://media-myr.b-cdn.net/images/resized/600/98d4e9e7-cf62-4e0a-b736-f49f9928c2db.webp"
         ],
         price: 43210,
-        mayarUrl: "https://gigital.myr.id/pl/ebook-gigital-panduan-peresepan-dokter-gigi-pasien-anak?"
+        mayarUrl: "https://gigital.myr.id/pl/ebook-gigital-panduan-peresepan-dokter-gigi-pasien-anak?",
+        samplePdf: "https://drive.google.com/file/d/1OWV5co837zyrlYPVvp0KvDWmA98DXoTT/preview"
+        
     },
 {
         id: 3,
@@ -45,7 +49,8 @@ const ebooks = [
         "https://media-myr.b-cdn.net/images/resized/600/3a6baa2d-38c9-43a0-90c3-167cad21b2b1.jpeg"
         ],
         price: 54321,
-        mayarUrl: "https://gigital.myr.id/pl/ebook-gigital-panduan-manajemen-dental-kehamilan?"
+        mayarUrl: "https://gigital.myr.id/pl/ebook-gigital-panduan-manajemen-dental-kehamilan?",
+        samplePdf: "https://drive.google.com/file/d/1Kf4JUQXhFtemVmweBMECM2_-t5XetSep/preview"
     },
 {
         id: 4,
@@ -61,7 +66,8 @@ const ebooks = [
         "https://media-myr.b-cdn.net/images/resized/600/92f1ad8d-e37a-47b7-a74a-0efe416af672.webp"
         ],
         price: 54321,
-        mayarUrl: "https://gigital.myr.id/pl/ebook-gigital-mudah-mendiagnosa-lesi-oral?"
+        mayarUrl: "https://gigital.myr.id/pl/ebook-gigital-mudah-mendiagnosa-lesi-oral?",
+        samplePdf: "https://drive.google.com/file/d/1MosJXbY77TUxWgnb6D7IRlcxEnmPLA9Y/preview"
     },
 {
         id: 5,
@@ -77,7 +83,8 @@ const ebooks = [
         "https://media-myr.b-cdn.net/images/resized/600/98236151-2996-436d-91be-5bf8e506b999.jpeg"
         ],
         price: 45678,
-        mayarUrl: "https://gigital.myr.id/pl/ebook-gigital-tatalaksana-lesi-oral?"
+        mayarUrl: "https://gigital.myr.id/pl/ebook-gigital-tatalaksana-lesi-oral?",
+        samplePdf: "https://drive.google.com/file/d/13qbIZHr351_klCBqM-SKwgSDPNuZtjU3/preview"
     },
 {
         id: 6,
@@ -93,6 +100,8 @@ const ebooks = [
         "https://media-myr.b-cdn.net/images/resized/600/40439849-a51a-4bbd-85d3-7b11a69bbb4a.jpeg"
         ],
         price: 45678,
-        mayarUrl: "https://gigital.myr.id/pl/ebook-gigital-panduan-pemilihan-obat-topikal?"
+        mayarUrl: "https://gigital.myr.id/pl/ebook-gigital-panduan-pemilihan-obat-topikal?",
+        samplePdf: "https://drive.google.com/file/d/1iIR_is9DAhEHJ26dExhVvY05k6D0bF5o/preview"
+
     }
 ];
